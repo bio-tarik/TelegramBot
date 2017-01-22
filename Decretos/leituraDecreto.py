91b2b32dc30d337526b960be80c0871f16227c41
Luiz Irber <github@luizirber.org>
2017-01-22 15:09:46 -0800

    Corrige alguns erros de sintaxe

--- Decretos/leituraDecreto.py
+++ Decretos/leituraDecreto.py
@@ -1,27 +1,28 @@
 # exec(open(leituraDecreto.py).read())
 import json, random, codecs
+import os
 
-def abrirArquivo()
-    with open('DecretoDB.ddg') as data_file
+def abrirArquivo():
+    with open(os.path.join(os.path.dirname(__file__), 'DecretoDB.ddg')) as data_file:
         data = json.load(data_file)
     return data
 
-def obterFrases()
+def obterFrases():
     data = abrirArquivo()
 
-    return data[decretos]
+    return data["decretos"]
 
-def sortearFrases(frases)
+def sortearFrases(frases):
     return random.sample(frases, 5)
 
-def addFrases(frase)
+def addFrases(frase):
     data = abrirArquivo()
     data[decretos].append(frase)
 
-    with codecs.open('DecretoDB.ddg', mode='w', encoding='utf-8') as data_file
+    with codecs.open('DecretoDB.ddg', mode='w', encoding='utf-8') as data_file:
         json.dump(data, data_file, ensure_ascii=False)
 
-def manipularMensagem(mensagem)
+def manipularMensagem(mensagem):
     return ' '.join(map(str.strip, mensagem))
 
 
